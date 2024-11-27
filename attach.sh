6a24a7d5a23f1bf40ee03bfaa94e27bdf5e3f0c3
James Fehrmann <jfehrmann@outlook.com>
2024-11-27 11:29:33 -0500

    better attach

--- attach.sh
+++ attach.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-id=`docker ps -a |head -2 |tail -1 |awk '{print $13}'`
+docker ps -a
+id=`docker ps -a |head -2 |tail -1 |awk '{print $NF}'`
+echo "Attempting to connect to ${id}"
 docker start ${id}
 docker exec -it ${id} bash
 
