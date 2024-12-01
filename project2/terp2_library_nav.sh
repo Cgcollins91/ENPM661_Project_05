dffba46701d8cf1caa31278b00c6dc7a2ce52ea9
aliebers0 <aliebers@umd.edu>
2024-12-01 17:56:29 -0500

    grab collect and place

--- project2/terp2_library_nav.sh
+++ project2/terp2_library_nav.sh
@@ -1,10 +1,71 @@
 #!/bin/bash
 
-./terp2_set_goal.sh -4 0
-sleep 15
-# arm action (book pickup)
+# go to table
+./terp2_set_goal.sh 9 0
+./terp2_set_arm.sh 3.0 0.0 0.0 0.0 0.0
+./terp2_set_gripper.sh 1.5 0.0 0.0 0.0
+sleep 12
 
-./terp2_set_goal.sh 0 -4
+# grab 1x book
+./terp2_set_arm.sh 3.0 0.5 1.25 -0.8 0.0
+sleep 2
+./terp2_set_gripper.sh 1.5 0.0 0.03 0.03
+sleep 1
+
+# store 1x book
+./terp2_set_arm.sh 3.0 0.0 -0.8 0.0 0.0
+sleep 1
+./terp2_set_arm.sh 3.0 -1.1 -1.1 0.0 -0.1
+sleep 1
+./terp2_set_arm.sh 3.0 -1.25 -0.9 -0.3 -0.1
+sleep 1
+./terp2_set_gripper.sh 1.5 0.0 0.0 0.0
+sleep 1
+./terp2_set_arm.sh 3.0 -1.1 -1.1 0.0 -0.1
+sleep 1
+./terp2_set_arm.sh 3.0 0.0 -0.8 0.0 0.0
+sleep 1
+
+# go to shelf
+./terp2_set_goal.sh 3 0
+./terp2_set_arm.sh 3.0 0.0 0.0 0.0 0.0
+sleep 10
+./terp2_set_goal.sh 0 6
 sleep 15
-# arm action (book dropoff)
+
+# remove 1x book from storage
+./terp2_set_arm.sh 3.0 0.0 -0.8 0.0 0.0
+sleep 1
+./terp2_set_arm.sh 3.0 -1.1 -1.1 0.0 -0.1
+sleep 1
+./terp2_set_arm.sh 3.0 -1.25 -0.9 -0.3 -0.1
+sleep 1
+./terp2_set_gripper.sh 1.5 0.0 0.03 0.03
+sleep 1
+./terp2_set_arm.sh 3.0 -1.1 -1.1 0.0 -0.1
+sleep 1
+./terp2_set_arm.sh 3.0 0.0 -0.8 0.0 0.0
+sleep 1
+./terp2_set_arm.sh 3.0 0.0 0.0 0.0 0.0
+sleep 1
+
+# place 1x book on shelf
+./terp2_set_arm.sh 2.7 0.0 0.0 0.0 0.0
+sleep 2
+./terp2_set_arm.sh 2.7 0.0 1.0 2.1 3.0
+sleep 2
+./terp2_set_arm.sh 2.7 0.2 1.2 2.2 3.0
+sleep 1
+./terp2_set_gripper.sh 1.5 0.0 0.0 0.0
+sleep 1
+./terp2_set_arm.sh 2.7 0.2 1.2 2.2 3.0
+sleep 2
+./terp2_set_arm.sh 2.7 0.0 0.0 0.0 0.0
+sleep 2
+
+# # go home
+./terp2_set_goal.sh 0 0
+sleep 10
+
+
 
